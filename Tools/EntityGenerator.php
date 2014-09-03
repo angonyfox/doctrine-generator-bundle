a42357f7e065736faf656cb41efaacdb8d6bbfc8
imptum <imptum@gmail.com>
2014-09-04 04:29:23 +0700

    Update EntityGenerator
    Change Entity class to extend from Model.

--- Tools/EntityGenerator.php
+++ Tools/EntityGenerator.php
@@ -38,6 +38,56 @@ class EntityGenerator extends Generator
         return str_replace('<spaces>', $this->spaces, $code);
     }
 
+    public function generateUpdatedEntityClass(ClassMetadataInfo $metadata, $path)
+    {
+        $currentCode = file_get_contents($path);
+
+        $placeHolders = array(
+            '<namespace>',
+            '<entityAnnotation>',
+            '<entityClassName>'
+        );
+
+        $replacements = array(
+            $this->generateEntityNamespace($metadata),
+            $this->generateEntityDocBlock($metadata),
+            $this->generateEntityClassName($metadata)
+        );
+        $modifiedTemplate = preg_replace("/\{(\s*.*\s*)\}\s*/i", "{", static::$classTemplate);
+
+        $code = str_replace($placeHolders, $replacements, $modifiedTemplate);
+
+        $first = strrpos($currentCode, '{');
+
+        return $code.substr($currentCode, $first+1);
+    }
+
+    protected function generateEntityNamespace(ClassMetadataInfo $metadata)
+    {
+        $lines = array();
+        if ($this->hasNamespace($metadata))
+        {
+            $lines[] = 'namespace ' . $this->getNamespace($metadata) .';';
+        }
+        if ($this->extendsClass())
+        {
+            $extendClassName = ($pos = strrpos($this->getClassToExtendName(), '\\')) ? substr($this->getClassToExtendName(), $pos+1, strlen($this->getClassToExtendName())) : $this->getClassToExtendName();
+            $lines[] = "";
+            $lines[] = 'use '.ltrim($this->getClassToExtendName(), '\\').' as Base'.$extendClassName.';';
+        }
+        return implode("\n", $lines);
+    }
+
+    protected function generateEntityClassName(ClassMetadataInfo $metadata)
+    {
+        if ($this->extendsClass())
+        {
+            $extendClassName = ($pos = strrpos($this->getClassToExtendName(), '\\')) ? 'Base'.substr($this->getClassToExtendName(), $pos+1, strlen($this->getClassToExtendName())) : $this->getClassToExtendName();
+        }
+        return 'class ' . $this->getClassName($metadata) .
+            ($this->extendsClass() ? ' extends ' . $extendClassName : null);
+    }
+
     public function writeClass(ClassMetadataInfo $metadata, $outputDirectory)
     {
         $this->setClassToExtend($metadata->rootEntityName);
